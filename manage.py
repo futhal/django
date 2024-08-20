9506acf0645cded0a68baeab81b4eb9cfc176660
futhal <75042617+futhal@users.noreply.github.com>
2024-08-20 21:07:50 +0000

    ddd

--- manage.py
+++ manage.py
@@ -15,7 +15,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv,"runserver", "8002")
+    
+    if len(sys.argv) == 1:
+        sys.argv += ['runserver', '8002']
+    
+    execute_from_command_line(sys.argv)
+
 
 
 if __name__ == '__main__':
